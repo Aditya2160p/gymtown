9c0aad63bd67bfd5aa3a8c5868c2cd3569fd4ee4
Aditya2160p <aditya.k0907@gmail.com>
2022-12-12 15:28:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from './Login/login';
+import { Route,Routes,Router, Link } from 'react-router-dom';
+import Home from './Home/home';
+//import './App.css'
+function App(){
+  return (
+    <Home/>
+  );
+}
+export default App;
